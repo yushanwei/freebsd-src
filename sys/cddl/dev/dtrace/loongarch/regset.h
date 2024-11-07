aa51dab7f6e9693e47938cc7151a756599b56f18
yu shan wei <mpysw@vip.163.com>
2024-11-07 09:10:45 +0800

    LoongArch:sys/cddl

--- sys/cddl/dev/dtrace/loongarch/regset.h
+++ sys/cddl/dev/dtrace/loongarch/regset.h
@@ -40,40 +40,39 @@
 extern "C" {
 #endif
 
-#define REG_ZERO	0
-#define REG_RA		1
-#define REG_SP		2
-#define REG_GP		3
-#define REG_TP		4
-#define REG_T0		5
-#define REG_T1		6
-#define REG_T2		7
-#define REG_S0		8
-#define REG_FP		8
-#define REG_S1		9
-#define REG_A0		10
-#define REG_A1		11
-#define REG_A2		12
-#define REG_A3		13
-#define REG_A4		14
-#define REG_A5		15
-#define REG_A6		16
-#define REG_A7		17
-#define REG_S2		18
-#define REG_S3		19
-#define REG_S4		20
-#define REG_S5		21
-#define REG_S6		22
-#define REG_S7		23
-#define REG_S8		24
-#define REG_S9		25
-#define REG_S10		26
-#define REG_S11		27
-#define REG_T3		28
-#define REG_T4		29
-#define REG_T5		30
-#define REG_T6		31
-#define REG_PC		32
+/* LoongArch Registers */
+#define REG_ZERO        0x0
+#define REG_RA          0x1
+#define REG_TP          0x2
+#define REG_SP          0x3
+#define REG_A0          0x4 /* Reused as V0 for return value */
+#define REG_A1          0x5 /* Reused as V1 for return value */
+#define REG_A2          0x6
+#define REG_A3          0x7
+#define REG_A4          0x8
+#define REG_A5          0x9
+#define REG_A6          0xa
+#define REG_A7          0xb
+#define REG_T0          0xc
+#define REG_T1          0xd
+#define REG_T2          0xe
+#define REG_T3          0xf
+#define REG_T4          0x10
+#define REG_T5          0x11
+#define REG_T6          0x12
+#define REG_T7          0x13
+#define REG_T8          0x14
+#define REG_U0          0x15 /* Kernel uses it as percpu base */
+#define REG_FP          0x16
+#define REG_S0          0x17
+#define REG_S1          0x18
+#define REG_S2          0x19
+#define REG_S3          0x1a
+#define REG_S4          0x1b
+#define REG_S5          0x1c
+#define REG_S6          0x1d
+#define REG_S7          0x1e
+#define REG_S8          0x1f
 
 #ifdef	__cplusplus
 }
